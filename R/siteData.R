534aa4e7cb4ab9e90b636bf2e431f5b057371589
Carl Vigren <carl.vigren@slu.se>
2021-04-03 16:12:16 +0200

    siteData completed
    siteData.R undergoing final corrections to create a siteData object.

--- R/siteData.R
+++ R/siteData.R
@@ -2,10 +2,14 @@
 #'
 #' @description Initiates a siteData object, containing information about the plot site.
 #'
+#' @param standid Stand ID
+#' @param distance_to_coast if there is no coordinate information
+#' @param aspect Main aspect (N,E,S,W or 0)
+#' @param incline degrees.
 #' @param altitude Metres above sea level
 #' @param latitude Latitude decimal WGS84
 #' @param longitude Longitude decimal WGS84
-#' @param polygon Optional List of boundary coordinates.
+#' @param polygon Optional List of boundary coordinates. Warning siteData does not check validity of polygon geometry.
 #' @param county County in Sweden. If not supplied, will calculate through \link[forester]{county_sweden}
 #' @param local_climate Swedish Local Climate Code. If not supplied, will calculate through \link[forester]{local_climate_sweden}
 #' @param vegetation NFI Vegetation code (FsKod) 1-18
@@ -17,13 +21,17 @@
 #' @export
 #'
 #' @examples
-siteData <- function(altitude, latitude, longitude, polygon, county, local_climate, vegetation, soil_texture, soil_moisture, temperature_sum.c, aspect,incline){
+siteData <- function(altitude, latitude, longitude, distance_to_coast, polygon, county, local_climate, vegetation, soil_texture, soil_moisture, temperature_sum.c, aspect,incline){
 
   if(missing(altitude)) stop("altitude is required")
   if(missing(latitude)) stop("latitude is required")
   if(missing(vegetation)) stop("vegetation is required")
   if(missing(soil_texture)) stop("soil_texture is required")
   if(missing(soil_moisture)) stop("soil_moisture is required")
+  if(!missing(polygon)){
+    message("Warning: Polygon geometry not validated by forester::siteData")
+  }
+
 
   if(missing(county)){
     county <- county_sweden(latitude = latitude,longitude = longitude)
@@ -33,9 +41,25 @@ siteData <- function(altitude, latitude, longitude, polygon, county, local_clima
     local_climate <- forester::local_climate_sweden(latitude = latitude,longitude = longitude)
   }
 
-  temperature_sum.c <- if(!exists(temperature_sum.c)){
-    forester::odin_tsum_1983(lat)
+  temperature_sum.c <- if(missing(temperature_sum.c)){
+    forester::odin_tsum_1983(latitude = latitude, altitude = altitude)
   }
+
+  if(missing(distance_to_coast)){
+    distance_to_coast <- forester::coast_distance(latitude = latitude, longitude = longitude)
+  }
+
+  if(missing(incline)){
+    incline <- 0
+    message("No incline given, setting incline to 0.")
+  }
+
+  if(missing(aspect)){
+    aspect <- 0
+    message("No aspect given, setting aspect to 0.")
+  }
+
+
 
 
   value <- list(main_species, altitude, latitude, longitude, polygon, county, local_climate, vegetation, soil_texture, soil_moister, temperature_sum.c, aspect, incline)
@@ -45,3 +69,4 @@ siteData <- function(altitude, latitude, longitude, polygon, county, local_clima
   value
 
 }
+
