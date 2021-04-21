0ab85bb5f430c67c688bca35ff4445be80310887
Carl Vigren <carl.vigren@slu.se>
2021-04-21 16:58:26 +0200

    Updated output params
    Included new output params.

--- R/standSummary.R
+++ R/standSummary.R
@@ -15,14 +15,27 @@ standSummary <- function(siteData,treelistData, managementData){
   Main_cohort_age <- treelistData %>% filter(generation==1) %>% summarise(max(age))
   Main_cohort_mean_height <- treelistData %>% filter(generation==1) %>% summarise(mean(height.m))
   Main_cohort_10percent_height <- treelistData %>% filter(generation==1) %>% summarise(quantile(height.m, 0.9))
+  Mean_diameter <- treelistData %>% summarise(mean(diameter.cm))
   stand_basal_area_per_ha <- treelistData %>% summarise(sum(basal_area)/area)
   basal_area_species_wise_per_ha <- treelistData %>% group_by(species) %>% summarise(sum(basal_area)/area)
+  basal_area_deciduous_per_ha <- basal_area_species_wise_per_ha %>% mutate(Deciduous= tree_type(species=species)) %>% filter(Deciduous=="Deciduous") %>% summarise(sum(basal_area)/area)
   N_species_wise_per_ha <- treelistData %>% filter(max(observation_year)) %>% group_by(species) %>% summarise(N = n()/siteData$area)
   N_total_trees_per_ha <- treelistData %>% filter(max(observation_year)) %>% summarise(N=n()/siteData$area)
   standing_volume_specieswise_per_ha <- treelistData %>% filter(max(observation_year)) %>% group_by(species) %>%  summarise(Volume=sum(volume)/area)
   management_list <- managementData %>% group_by(management_event, year) %>% summarise(m3sk = sum(volume))
 
-  stand_summary <- list(StandID, Main_cohort_age, Main_cohort_mean_height,Main_cohort_10percent_height,N_species_wise_per_ha,N_total_trees_per_ha,standing_volume_specieswise_per_ha,management_list)
+  stand_summary <- list(StandID,
+                        Main_cohort_age,
+                        Main_cohort_mean_height,
+                        Main_cohort_10percent_height,
+                        Mean_diameter,
+                        stand_basal_area_per_ha,
+                        basal_area_species_wise_per_ha,
+                        basal_area_deciduous_per_ha,
+                        N_species_wise_per_ha,
+                        N_total_trees_per_ha,
+                        standing_volume_specieswise_per_ha,
+                        management_list)
   class(stand_summary) <- c("list","standSummary")
 
   return(stand_summary)
