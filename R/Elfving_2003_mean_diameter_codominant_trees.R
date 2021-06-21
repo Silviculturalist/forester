a49d800707cf4adc87d3be0d649f6436df3cc354
Carl Vigren <carl.vigren@slu.se>
2021-06-21 09:00:34 +0200

    Export
    Ensure that Elfving 2003 mean diameter of co-dominant trees is properly exported.

--- R/Elfving_2003_mean_diameter_codominant_trees.R
+++ R/Elfving_2003_mean_diameter_codominant_trees.R
@@ -5,11 +5,12 @@
 #' @param total_stand_age total stand age
 #' @param SIS site index according site factors, given as top height at age 100, m.
 #' @param total_ba As estimated with relascope in field. m^2 / ha
+#' @export
 #'
 #' @return
 #'
 #'
-mean_diam_codominant <- function(total_stand_age, SIS, total_ba){
+Elfving_2003_mean_diameter_codominant_trees <- function(total_stand_age, SIS, total_ba){
   lnd <- -0.9231 + 1.0032*log(total_stand_age) - 0.00701*total_stand_age - 4.005/SIS + 0.0186*SIS - 1.882/total_ba + 0.036
 
   return(lnd)
