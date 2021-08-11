bc345ccf90646c42985d7f5e82e640715246f6ac
Carl Vigren <carl.vigren@slu.se>
2021-08-11 15:19:52 +0200

    Leijon 1979 translation between SI H100 Pine Spruce on same plot
    Initial commit Leijon 1979.

---
+++ R/Leijon_1979_SI_translation_Pine_to_Spruce_1.R
@@ -0,0 +1,41 @@
+#' Height trajectories of Norway Spruce and Scots Pine on equal sites.
+#'
+#' @source Leijon, B. (1979) Tallen och Granens produktion på lika ståndort. Redovisning till SJFR. SLU, Inst. f. skogsskötsel, intern rapport 100pp.
+#'
+#' Funktion 7.2, p. 63.
+#'
+#' @details
+#' Standard deviation, approximately reduced for observational error in H100: 0.1070.
+#'
+#'
+#' Standard deviation of unlogarithmed residuals (dm) 38.4.
+#'
+#' @param SI_Pine SI Pine H100, meters by Hägglund 1974.
+#'
+#' @return SI Spruce H100, meters by Hägglund 1972,1973.
+#'
+#' @seealso
+#' [forester::Leijon_1979_SI_translation_Pine_to_Spruce_3]
+#' [forester::Leijon_1979_SI_translation_Pine_to_Spruce_2]
+#' [forester::Leijon_1979_SI_translation_Spruce_to_Pine_1]
+#' [forester::Leijon_1979_SI_translation_Spruce_to_Pine_2]
+#' [forester::Leijon_1979_SI_translation_Spruce_to_Pine_3]
+#' @export
+#'
+#' @examples
+Leijon_1979_SI_translation_Pine_to_Spruce_1 <- function(
+  SI_Pine
+){
+
+  if(SI_Pine<8|SI_Pine>30){
+    warning("SI Pine may be outside underlying material. Minimum SI Spruce at 8.2 m SI Pine.")
+  }
+
+  return(
+    exp(
+      -0.9596*log(SI_Pine*10)+
+        +0.01171*(SI_Pine*10)+
+        +7.9209 #approximately corrected for logarithmic bias.
+    )/10
+  )
+}
---
+++ R/Leijon_1979_SI_translation_Pine_to_Spruce_2.R
@@ -0,0 +1,49 @@
+#' Height trajectories of Norway Spruce and Scots Pine on equal sites.
+#'
+#' @source Leijon, B. (1979) Tallen och Granens produktion på lika ståndort. Redovisning till SJFR. SLU, Inst. f. skogsskötsel, intern rapport 100pp.
+#'
+#' Funktion 7.4, p. 66.
+#'
+#' @details
+#' Standard deviation, approximately reduced for observational error in H100: 0.0812.
+#'
+#'
+#' Standard deviation of unlogarithmed residuals (dm) 27.3.
+#'
+#' @param SI_Pine SI Pine H100, meters by Hägglund 1974.
+#' @param latitude Degrees N.
+#' @param altitude Meters above sea level.
+#' @param continental TRUE/FALSE Is the plot located in a locally continental climate. [forester::local_climate_sweden()]
+#'
+#' @return SI Spruce H100, meters by Hägglund 1972,1973.
+#'
+#' @seealso
+#' [forester::Leijon_1979_SI_translation_Pine_to_Spruce_1]
+#' [forester::Leijon_1979_SI_translation_Pine_to_Spruce_3]
+#' [forester::Leijon_1979_SI_translation_Spruce_to_Pine_1]
+#' [forester::Leijon_1979_SI_translation_Spruce_to_Pine_2]
+#' [forester::Leijon_1979_SI_translation_Spruce_to_Pine_3]
+#' @export
+#'
+#' @examples
+Leijon_1979_SI_translation_Pine_to_Spruce_2 <- function(
+  SI_Pine,
+  latitude,
+  altitude,
+  continental
+){
+
+  if(SI_Pine<12|SI_Pine>30){
+    warning("SI H100 Pine Outside of material.")
+  }
+
+  return(
+    exp(
+      +0.005217*(SI_Pine*10)+
+      -1.3914*log(latitude)+
+      -0.01114*(altitude/100)+
+      -0.1024*continental+
+      +9.9936
+    )/10
+  )
+}
