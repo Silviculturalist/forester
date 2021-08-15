538d3d0a5a9d791b494d1c2a92e2e18ad7918dbd
Carl Vigren <carl.vigren@slu.se>
2021-08-15 16:54:07 +0200

    Correction compound interest Carbonnier BA
    function corrected.

--- R/Carbonnier_1975_compound_basal_area_growth_Oak.R
+++ R/Carbonnier_1975_compound_basal_area_growth_Oak.R
@@ -36,16 +36,20 @@ Carbonnier_1975_compound_basal_area_growth_Oak <- function(
   removal_third_last_thinning_BA_understory
 
 ){
+
   return(
   -0.7804+
-  +0.1728*((10^3)/total_age)+
-  +0.7061*((10^2)/mean_basal_area_diameter_after_thinning)-
+  +0.1728*((1000)/total_age)+
+  +0.7061*((100)/mean_basal_area_diameter_after_thinning)+
   -0.1408*((mean_height_Lorey_after_thinning*sqrt(stems_after_thinning))/mean_basal_area_diameter_after_thinning)+
   +0.1841*10*sqrt(
     (((removal_last_thinning_BA+removal_last_thinning_BA_understory)/2)+
-      removal_second_last_thinning_BA+removal_second_last_thinning_BA_understory +
+      (removal_second_last_thinning_BA+removal_second_last_thinning_BA_understory) +
       ((removal_third_last_thinning_BA+removal_third_last_thinning_BA_understory)/2))/(BA_before_thinning+BA_before_thinning_understory)
   )+
   -0.0945*((10*SIH100)/silt_fraction)
   )
 }
+
+
+
