088cf0782a393acd2e570156197323036c9d407e
Carl Vigren <carl.vigren@slu.se>
2021-09-11 13:41:06 +0200

    Soderberg 1986 Pine Northern Sweden BA increment
    Init commit Soderberg 1986 Pine BA increment N Sweden

---
+++ R/Soderberg_1986_BA_increment_northern_Sweden_over_45_Pine.R
@@ -0,0 +1,156 @@
+#' 5 year Basal area increment over bark, Pine, northern Sweden, over 45 years at breast height from Söderberg (1986)
+#'
+#' @source Söderberg, U. (1986) Funktioner för skogliga produktionsprognoser - Tillväxt och formhöjd för enskilda träd av inhemska trädslag i Sverige. / Functions for forecasting of timber yields - Increment and form height for individual trees of native species in Sweden. Report 14. Section of Forest Mensuration and Management. Swedish University of Agricultural Sciences. Umeå. ISBN 91-576-2634-0. ISSN 0349-2133. pp.251. p. 168.
+#'
+#' @description
+#' \strong{Applicable counties:}
+#'
+#' *Norrbottens Lappmark
+#' *Norrbottens kustland
+#' *Västerbottens lappmark
+#' *Västerbottens kustland
+#' *Västernorrland - Ångermanlands landskap
+#' *Västernorrland - Medelpads landskap
+#' *Jämtland - Jämtlands landskap
+#' *Jämtland - Härjedalens landskap
+#' *Kopparberg - Sälen-Idre.
+#'
+#' @details
+#' Multiple correlation coefficient R = 0.80
+#'
+#' Spread about the function sf = 0.502
+#'
+#' sf/Spread about the mean = 0.60
+#'
+#' Number of observations = 2495
+#'
+#' Sum of weights: 2351.3
+#'
+#' NB in Söderberg (1986), no correction for logarithmic bias was introduced, as, (freely translated) p. 114:
+#' "At the presentation of the functions we lightly touched on the effects of the measurement
+#' errors in the variables which had been included in the regression. It was then concluded
+#' that several factors contrived to that the spread about the functions is overestimated.
+#' Therefore no correction for logarithmic bias was carried out, primarily because the
+#' effect of the errors in the indipendent variables on the spread about the function
+#' are difficult to establish. If one were to assume that the spread about the function
+#' was overestimated by 10 percent, the predicted growth from the functions would be increased by
+#' roughly 2 percent. For 20 percents overestimation, an increase of predicted growth by 4.5 perpcent."
+#'
+#'
+#'
+#'
+#' @param diameter_cm Diameter at breast height
+#' @param diameter_largest_tree_on_plot_cm Diameter at breast height of the largest tree on the plot.
+#' @param Basal_area_of_tree_m2 Basal area of the tree, m^2.
+#' @param Basal_area_Spruce_m2_ha Basal area Spruce on the plot, m^2 / ha.
+#' @param Basal_area_Birch_m2_ha Basal area Birch on the plot, m^2 /ha.
+#' @param Basal_area_plot_m2_ha Basal area of all tree species the plot, m^2 / ha.
+#' @param age_at_breast_height Age at breast height of the tree.
+#' @param thinned TRUE if the stand has been thinned, otherwise FALSE.
+#' @param last_thinned Number of growing seasons since last thinning.
+#' @param SI100 Site Index H100, m.
+#' @param SI_species Species for which SIH100 was estimated. One of : 'Picea abies' or 'Pinus sylvestris'.
+#' @param latitude Latitude, degrees.
+#' @param altitude altitude, metres above sea level.
+#' @param aspect Aspect, one of: "north", "south" or 0.
+#' @param soil_moisture Type 1="Dry/torr",2="Mesic/frisk",3="Mesic-moist/frisk-fuktig",4="Moist/fuktig",5="Wet/Blöt"
+#' @param peatland 1 if plot is Peatland, 0 for others (default).
+#' @param divided_plot 1 for plots described in different parts, which appears when the original plot consists of different land classes, density classes or cutting classes or belongs to different owners. 0 for full plots (default).
+#' @param fertilised_plot 1 for fertilised plots, 0 for others (default).
+#' @param plot_inventoried_76_77 1 for plots measured in the years 1976-77, 0 for others (default).
+#'
+#' @return Basal Area increment during 5 years, m2.
+#' @export
+#'
+#' @examples
+Soderberg_1986_BA_increment_northern_Sweden_over_45_Pine <- function(
+  diameter_cm,
+  diameter_largest_tree_on_plot_cm,
+  Basal_area_of_tree_m2,
+  Basal_area_Spruce_m2_ha,
+  Basal_area_Birch_m2_ha,
+  Basal_area_plot_m2_ha,
+  age_at_breast_height,
+  thinned,
+  last_thinned,
+  SI100,
+  SI_species,
+  latitude,
+  altitude,
+  aspect,
+  soil_moisture,
+  peatland=0,
+  divided_plot=0,
+  fertilised_plot=0,
+  plot_inventoried_76_77=0
+){
+  basal_area_of_tree_cm2 <- Basal_area_of_tree_m2*10000
+  north <- ifelse(aspect=="north",1,0)
+  south <- ifelse(aspect=="south",1,0)
+  spruce <- ifelse(species=="Picea abies")
+  pine <- ifelse(species=="Pinus sylvestris")
+  dry <- ifelse(soil_moisture==1,1,0)
+  diameter_quotient <- diameter_cm/diameter_largest_tree_on_plot_cm
+  BA_quotient_Spruce <- Basal_area_Spruce_m2_ha/Basal_area_plot_m2_ha
+  BA_quotient_Birch <- Basal_area_Birch_m2_ha/Basal_area_plot_m2_ha
+
+  thinning <- ifelse(thinned==FALSE,
+                     (#unthinned
+                       -0.62615E-01*Basal_area_plot_m2_ha+
+                         +0.70095E-03*(Basal_area_plot_m2_ha^2)
+
+                     )
+
+                     ,
+
+                     ifelse(thinned==TRUE & last_thinned<5,
+                            (#thinned within 5 years
+                              -0.48420E-01*Basal_area_plot_m2_ha+
+                                +0.46192E-03*(Basal_area_plot_m2_ha^2)
+                            )
+
+
+                            ,
+
+                            (#thinned longer ago than 5 years.
+                              -0.52634E-01*Basal_area_plot_m2_ha+
+                                +0.53348E-03*(Basal_area_plot_m2_ha^2)
+
+                            )
+
+
+                     )
+
+  )
+
+
+
+  return(
+    exp(
+      +0.10633E+01*log(basal_area_of_tree_cm2)+
+        -0.53052E-03*basal_area_of_tree_cm2+
+        +0.15859E+03*(1/(age_at_breast_height+10))+
+        -0.36056E+04*((1/(age_at_breast_height+10))^2)+
+        thinning+
+        -0.31676E+00*diameter_quotient+
+        +0.28762E+00*BA_quotient_Spruce+
+        +0.34134E+00*BA_quotient_Birch+
+        +0.30746E-02*spruce*SI100*10+#m to dm.
+        +0.27871E-02*pine*SI100*10+ #m to dm.
+        +0.39932E+00*peatland+
+        -0.61609E-01*latitude+
+        -0.76913E-02*altitude+
+        +0.11708E-03*latitude*altitude+
+        -0.53450E-01*north+
+        +0.53028E-01*south+
+        -0.48108E-01*dry+
+        +0.14839E+00*divided_plot+
+        +0.12531E+00*fertilised_plot+
+        +0.10107E+00*plot_inventoried_76_77+
+        +0.63632E+00
+
+    )/10000 #cm2 to m2
+  )
+
+
+}
---
+++ R/Soderberg_1986_BA_increment_northern_Sweden_under_65_Pine.R
@@ -0,0 +1,147 @@
+#' 5 year Basal area increment over bark, Pine, northern Sweden, under 65 years at breast height from Söderberg (1986)
+#'
+#' @source Söderberg, U. (1986) Funktioner för skogliga produktionsprognoser - Tillväxt och formhöjd för enskilda träd av inhemska trädslag i Sverige. / Functions for forecasting of timber yields - Increment and form height for individual trees of native species in Sweden. Report 14. Section of Forest Mensuration and Management. Swedish University of Agricultural Sciences. Umeå. ISBN 91-576-2634-0. ISSN 0349-2133. pp.251. p. 166.
+#'
+#' @description
+#' \strong{Applicable counties:}
+#'
+#' *Norrbottens Lappmark
+#' *Norrbottens kustland
+#' *Västerbottens lappmark
+#' *Västerbottens kustland
+#' *Västernorrland - Ångermanlands landskap
+#' *Västernorrland - Medelpads landskap
+#' *Jämtland - Jämtlands landskap
+#' *Jämtland - Härjedalens landskap
+#' *Kopparberg - Sälen-Idre.
+#'
+#' @details
+#' Multiple correlation coefficient R = 0.88
+#'
+#' Spread about the function sf = 0.419
+#'
+#' sf/Spread about the mean = 0.48
+#'
+#' Number of observations = 1249
+#'
+#' Sum of weights: 1084.2
+#'
+#' NB in Söderberg (1986), no correction for logarithmic bias was introduced, as, (freely translated) p. 114:
+#' "At the presentation of the functions we lightly touched on the effects of the measurement
+#' errors in the variables which had been included in the regression. It was then concluded
+#' that several factors contrived to that the spread about the functions is overestimated.
+#' Therefore no correction for logarithmic bias was carried out, primarily because the
+#' effect of the errors in the indipendent variables on the spread about the function
+#' are difficult to establish. If one were to assume that the spread about the function
+#' was overestimated by 10 percent, the predicted growth from the functions would be increased by
+#' roughly 2 percent. For 20 percents overestimation, an increase of predicted growth by 4.5 perpcent."
+#'
+#'
+#'
+#'
+#' @param diameter_cm Diameter at breast height
+#' @param diameter_largest_tree_on_plot_cm Diameter at breast height of the largest tree on the plot.
+#' @param Basal_area_of_tree_m2 Basal area of the tree, m^2.
+#' @param Basal_area_Spruce_m2_ha Basal area Spruce on the plot, m^2 / ha.
+#' @param Basal_area_plot_m2_ha Basal area of all tree species the plot, m^2 / ha.
+#' @param age_at_breast_height Age at breast height of the tree.
+#' @param thinned TRUE if the stand has been thinned, otherwise FALSE.
+#' @param last_thinned Number of growing seasons since last thinning.
+#' @param SI100 Site Index H100, m.
+#' @param SI_species Species for which SIH100 was estimated. One of : 'Picea abies' or 'Pinus sylvestris'.
+#' @param latitude Latitude, degrees.
+#' @param aspect Aspect, one of: "north", "south" or 0.
+#' @param soil_moisture Type 1="Dry/torr",2="Mesic/frisk",3="Mesic-moist/frisk-fuktig",4="Moist/fuktig",5="Wet/Blöt"
+#' @param divided_plot 1 for plots described in different parts, which appears when the original plot consists of different land classes, density classes or cutting classes or belongs to different owners. 0 for full plots (default).
+#' @param fertilised_plot 1 for fertilised plots, 0 for others (default).
+#' @param plot_inventoried_76_77 1 for plots measured in the years 1976-77, 0 for others (default).
+#'
+#' @return Basal area increment during 5 years, m2.
+#' @export
+#'
+#' @examples
+Soderberg_1986_BA_increment_northern_Sweden_under_65_Pine <- function(
+  diameter_cm,
+  diameter_largest_tree_on_plot_cm,
+  Basal_area_of_tree_m2,
+  Basal_area_Spruce_m2_ha,
+  Basal_area_plot_m2_ha,
+  age_at_breast_height,
+  thinned,
+  last_thinned,
+  SI100,
+  SI_species,
+  latitude,
+  aspect,
+  soil_moisture,
+  divided_plot=0,
+  fertilised_plot=0,
+  plot_inventoried_76_77=0
+){
+  basal_area_of_tree_cm2 <- Basal_area_of_tree_m2*10000
+  north <- ifelse(aspect=="north",1,0)
+  south <- ifelse(aspect=="south",1,0)
+  spruce <- ifelse(species=="Picea abies")
+  pine <- ifelse(species=="Pinus sylvestris")
+  moist <- ifelse(soil_moisture>3,1,0)
+  diameter_quotient <- diameter_cm/diameter_largest_tree_on_plot_cm
+  BA_quotient_Spruce <- Basal_area_Spruce_m2_ha/Basal_area_plot_m2_ha
+
+  thinning <- ifelse(thinned==FALSE,
+                     (#unthinned
+                       -0.66243E-01*Basal_area_plot_m2_ha+
+                       +0.75292E-03*(Basal_area_plot_m2_ha^2)
+
+                     )
+
+                     ,
+
+                     ifelse(thinned==TRUE & last_thinned<5,
+                            (#thinned within 5 years
+                              -0.66400E-01*Basal_area_plot_m2_ha+
+                              +0.89623E-03*(Basal_area_plot_m2_ha^2)
+                            )
+
+
+                            ,
+
+                            (#thinned longer ago than 5 years.
+                              -0.54112E-01*Basal_area_plot_m2_ha+
+                              +0.43791E-03*(Basal_area_plot_m2_ha^2)
+
+                            )
+
+
+                            )
+
+                     )
+
+
+
+  return(
+    exp(
+      +0.12199E+01*log(basal_area_of_tree_cm2)+
+      -0.12143E-02*basal_area_of_tree_cm2+
+      -0.40367E+01*(log(basal_area_of_tree_cm2)/(age_at_breast_height+10))+
+      +0.77769E+02*(1/(age_at_breast_height+10))+
+      -0.19198E+03*((1/(age_at_breast_height+10))^2)+
+      thinning+
+      -0.17016E+00*diameter_quotient+
+      +0.31920E+00*BA_quotient_Spruce+
+      +0.24518E-02*spruce*SI100*10+#m to dm.
+      +0.23361E-02*pine*SI100*10+ #m to dm.
+      +0.13028E+01*latitude+
+      -0.99353E-02*(latitude^2)+
+      -0.56156E-01*north+
+      +0.12153E+00*south+
+      +0.15605E+00*moist+
+      +0.92899E-01*divided_plot+
+      +0.99817E-01*fertilised_plot+
+      +0.70382E-01*plot_inventoried_76_77+
+      -0.45954E+02
+
+    )/10000 #cm2 to m2
+  )
+
+
+}
