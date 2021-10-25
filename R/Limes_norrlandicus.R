95a856db4dbe38cdfbcb477375bcba5da037354f
Carl Vigren <carl.vigren@slu.se>
2021-10-25 17:29:39 +0200

    Limes norrlandicus
    Digitalised SpatialPolygonsDataFrame area north of Limes norrlandicus from fig. 2.2 in Tegnhammar 1992. Note: Not possible to save SpatVector or SpatRaster to rdata.

---
+++ R/Limes_norrlandicus.R
@@ -0,0 +1,11 @@
+#' Norrland_shape_file
+#'
+#' @source Roughly Digitalised from Tegnhammar, L. (1992) "Om skattningen av ståndortsindex för gran",
+#' eng:"On the estimation of site index for Norway spruce". Report 53. Dept. of
+#' Forest Survey. Swedish University of Agricultural Sciences. Umeå. p.41.
+#' ISSN 0348-0496
+#'
+#' @format A terra::SpatVector
+#'
+#' @description A polygon deligning an area including Sweden which is north of 'Limes norrlandicus'.
+"Limes_norrlandicus"
