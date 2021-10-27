82b29f563a94248dd92ad5230b11f840d965e3dd
Carl Vigren <carl.vigren@slu.se>
2021-10-27 11:14:28 +0200

    Coast distance
    Major overhaul utilising internal data forester:::clipped. A SpatialPolygonsDataFrame with only the Swedish coastline to significantly speed up process and ensure that only the distance to the Swedish coast is calculated.

--- R/coast_distance.R
+++ R/coast_distance.R
@@ -1,29 +1,23 @@
-#' Distance to Coast
+#' Distance to Swedish Coast
 #'
-#' @param latitude A decimal latitude WGS84.
-#' @param longitude A decimal longitude WGS84.
-#' @param detail Determines resolution of Coastline. 'medium' or 'large'. See rnaturalearth::ne_coastline for detail.
-#' @param unit 'm' or 'km'.
+#' @param latitude A decimal latitude.
+#' @param longitude A decimal longitude.
+#' @param epsg The EPSG code of the projection, e.g. for WGS84: '4326' (default). Or for SWEREF99TM: '3006'.
 #'
-#' @description Wrapper for sf::st_distance to measure distance between input coordinates and Natural Earth Coastline rnaturalearth::ne_coastline.
-#' For best results, use detail: 'large'. For faster processing time, use 'medium' or 'low'.
-#' @return Distance to coast in km.
+#'
+#' @description Wrapper for sf::st_distance to measure distance between input coordinates and Natural Earth Coastline rnaturalearth::ne_coastline of medium resolution.
+#' The world coastline set is very coarsely cut to include mainly Swedish coast.
+#'
+#'
+#' @return Distance to coast, km.
 #' @export
 #'
 #' @examples
 #' coast_distance(56.8,14.8)
-coast_distance <- function(latitude,longitude, detail='large', unit='km'){
-  world_coastline <- rnaturalearth::ne_coastline(scale=detail,'sf') %>% sf::st_transform(4326)
+coast_distance <- function(latitude,longitude, epsg=4326){
+
   site_spatial_point_df <- data.frame(`lat`=latitude,`long`=longitude)
-  site_spatial_point <- site_spatial_point_df %>% sf::st_as_sf(coords=c("long","lat")) %>% sf::st_set_crs(4326)
-
-  dist <- min(sf::st_distance(site_spatial_point, world_coastline))
-
-  dist <- as.numeric(dist)
-
-  if(unit=='km'){
-    dist <- dist/1000
-  }
-
-  return(dist)
+  site_spatial_point <- site_spatial_point_df %>% sf::st_as_sf(coords=c("long","lat")) %>% sf::st_set_crs(epsg)
+  dist <- min(sf::st_distance(site_spatial_point, forester:::clipped))
+  return((as.numeric(dist)/1000))
 }
