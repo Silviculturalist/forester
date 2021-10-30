f8e821745ab61bd0da4e155a658c0c9bf5fee1ba
Carl Vigren <carl.vigren@slu.se>
2021-10-30 18:51:10 +0200

    Eriksson_1976_self_thinning
    Added correcting factor as per p. 107 from Eriksson 1976.

--- R/Eriksson_1976_self_thinning.R
+++ R/Eriksson_1976_self_thinning.R
@@ -2,17 +2,20 @@
 #' @source From p. 86-88 in Eriksson, H. (1976) "Granens produktion i Sverige", translated: "Yield of Norway spruce in Sweden". Report no. 41. Dept. of Forest Yield Research. Royal College of Forestry. Stockholm.
 #' @description Author recommends that the function not be used when stems per ha exceed 4500 and dominant height at the same time is more than 15 m. Author still sees this as the best available information when it was written.
 #' @param number_trees_per_ha_period_start Number of trees per hectare at the start of the period.
-#' @param dominant_height_dm Dominant height, dm.
-#' @param SI Site Index H100 Spruce from Hägglund 1972/1973.
+#' @param dominant_height_m Dominant height, m.
+#' @param SI100 Site Index H100 Spruce from Hägglund 1972/1973, e.g. [forester::Hagglund_1972_northern_Sweden_Height_trajectories_Spruce()], [forester::Hagglund_1973_southern_Sweden_Height_trajectories_Spruce()]
 #'
 #' @return Annual self thinning in basal area over bark per hectare, m2.
 #' @export
 #'
 #' @examples
-Eriksson_1976_self_thinning <- function(number_trees_per_ha_period_start, dominant_height_dm, SI){
+Eriksson_1976_self_thinning <- function(number_trees_per_ha_period_start, dominant_height_m, SI){
+
+  dominant_height_dm <- dominant_height_m*10
 
   if(number_trees_per_ha_period_start>4500 && dominant_height_dm>150){
-    stop("Correction needed to Ekö PM self-thinning function. Needs correction from p. 107.")
+    warning("Correction needed to Ekö PM self-thinning function. Using Eriksson 1976 correction from p. 107.")
+    number_trees_per_ha_period_start <- 4500 + 0.1*(number_trees_per_ha_period_start-4500)
   }
 
 
