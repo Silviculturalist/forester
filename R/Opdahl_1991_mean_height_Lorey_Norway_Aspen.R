055153d9f59ea748e79d7836d4b270d6e7da92ec
Carl Vigren <carl.vigren@slu.se>
2021-11-27 13:59:39 +0100

    Opdahl 1991
    Function for Lorey's mean height and volume of individual trees not functional.

--- R/Opdahl_1991_mean_height_Lorey_Norway_Aspen.R
+++ R/Opdahl_1991_mean_height_Lorey_Norway_Aspen.R
@@ -11,7 +11,7 @@ Opdahl_1991_mean_height_Lorey_Norway_Aspen <- function(
 
   return(
     #Observe, contains typo in source. Missing a closing bracket.
-    (dominant_height-(-13.7396 + (dominant_height*1.58856)-((dominant_height^2)*0.00378)-(BA_mean_diameter*0.41154))+((BA_mean_diameter*dominant_height)*0.01406)+((stems_per_ha_before_thinning*dominant_height)*0.00007)-((Basal_area_m2_ha_before_thinning^2)*0.00152)+(stand_age_at_breast_height*0.01254)) + correction
+    (dominant_height-(-13.7396 + (dominant_height*1.58856)-((dominant_height^2)*0.00378)-(BA_mean_diameter*0.41154))+((BA_mean_diameter*dominant_height)*0.01406)+((stems_per_ha_before_thinning*dominant_height)*0.00007)-((Basal_area_m2_ha_before_thinning^2)*0.00152)+(stand_age_at_breast_height*0.01254)) - correction
   )
 
 }
