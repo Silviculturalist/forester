96b22a5fc43bd9e8d0c48519d536e40beda82871
Carl Vigren <carl.vigren@slu.se>
2021-11-28 14:18:11 +0100

    QMD according to Curtis & Mashall 2000
    QMD added year of publication.

--- R/quadratic_mean_diameter.R
+++ R/quadratic_mean_diameter.R
@@ -1,6 +1,6 @@
 #' Calculate the Quadratic Mean Diameter of a stand, cm.
 #'
-#' @source Curtis, R.O., Marshall, D.D. Technical Note: Why Quadratic Mean Diameter? West. J. Appl. For. 15(3):137-139. Available: \url{https://www.fs.fed.us/pnw/olympia/silv/publications/opt/436_CurtisMarshall2000.pdf}
+#' @source Curtis, R.O., Marshall, D.D. 2000. Technical Note: Why Quadratic Mean Diameter? West. J. Appl. For. 15(3):137-139. Available: \url{https://www.fs.fed.us/pnw/olympia/silv/publications/opt/436_CurtisMarshall2000.pdf}
 #'
 #' @param Basal_area_m2_ha Basal area m2 / ha.
 #' @param stems_per_ha Stems per ha.
