389b12854879f02e88915271c0c147e611bb325e
Carl Vigren <carl.vigren@slu.se>
2021-12-22 11:44:10 +0100

    Added .onLoad()
    added .onLoad() message.

---
+++ R/forester_startup_message.R
@@ -0,0 +1,3 @@
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage("forester is being actively developed.\nFor News and issues, go to 'https://github.com/Silviculturalist/forester'\nBreaking changes can take place at any time.")
+}
