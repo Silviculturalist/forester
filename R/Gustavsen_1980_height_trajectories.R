20262f93df958b5b5b1bd82afe549c363d1d1415
Carl Vigren <carl.vigren@slu.se>
2022-03-12 15:50:27 +0100

    Finnish height trajectories and SIS estimation
    Gustavsen 1980 height trajectories for Scots Pine and Norway Spruce in Finland. Heiskanen et al 2018 site index H100 from site factors estimation.

---
+++ R/Gustavsen_1980_height_trajectories.R
@@ -0,0 +1,102 @@
+#' Dominant height trajectories for Scots Pine and Norway Spruce in Finland
+#' from Gustavsen, 1980.
+#' @source Gustavsen, H-G (1980). Talousmetsien kasvupaikkaluokittelu
+#' valtapituuden avulla. (Site index curves for conifer stands in Finland.)
+#' Folia Forestalia 454: 1-31. Available (12/03/2021):
+#' \url{https://jukuri.luke.fi/handle/10024/522037}
+#'
+#' @description "Site index curves for natural Scots Pine.. and Norway spruce
+#' stands in Finland, based on 1069 temporary sample plots of 0.1 ha in pine-dominated
+#' and 617 plots in spruce-dominated stands from the 3rd national forest inventory
+#' (1951-53)".
+#'
+#' @param dominant_height Dominant height, in metres.
+#' @param age Total age
+#' @param age2 Total age at output.
+#' @name gustavsen1980
+#' @export
+Gustavsen_1980_height_trajectory_Finland_Scots_Pine <- function(
+    dominant_height,
+    age,
+    age2
+    ){
+  #Internal calc SI100
+  Gustavsen_1980_SI100_Finland_Scots_Pine <- function(age,dominant_height){
+    nim=log(dominant_height)-log(128.229)
+    sel=exp((4.70248/(age^0.47692))-(4.70248/(100^0.47692)))
+    return(
+      128.229*(exp(nim/sel))
+    )
+  }
+
+  SI100 <- Gustavsen_1980_SI100_Finland_Scots_Pine(age = {age},dominant_height = {dominant_height})
+
+  dominant_height <- {dominant_height}
+
+  hd_df <- data.frame(age=numeric(),height=numeric(),increment=numeric())
+  for(i in age:age2){
+    increment = (dominant_height * log(SI100/128.229) * exp(4.70248/i^0.47692 - 4.70248/100^0.47692) * (-2.24269/i^1.47692))
+    hd_df <- bind_rows(hd_df,data.frame("age"=i,"height"=dominant_height,"increment"=increment))
+    dominant_height <- if(age<age2) (dominant_height+increment) else (dominant_height-increment)
+  }
+
+  return(
+    hd_df[,"height"][nrow(hd_df)]
+  )
+}
+
+#' @export
+#' @rdname gustavsen1980
+Gustavsen_1980_height_trajectory_Finland_Norway_Spruce <- function(
+    dominant_height,
+    age,
+    age2
+){
+
+  #Internal calc SI100
+  Gustavsen_1980_SI100_Finland_Norway_Spruce <- function(age,dominant_height){
+    nim=log(dominant_height)-log(147.481)
+    sel=exp((4.64631/(age^0.29981))-(4.64631/(100^0.29981)))
+    return(
+      147.481*(exp(nim/sel))
+    )
+  }
+
+  SI100 <- Gustavsen_1980_SI100_Finland_Norway_Spruce(age = {age},dominant_height = {dominant_height})
+
+  dominant_height <- {dominant_height}
+
+  hd_df <- data.frame(age=numeric(),height=numeric(),increment=numeric())
+  for(i in age:age2){
+    increment = (dominant_height * log(SI100/147.481) * exp(4.64631/i^0.29981 - 4.64631/100^0.29981) * (-1.39299/i^1.29981))
+    hd_df <- bind_rows(hd_df,data.frame("age"=i,"height"=dominant_height,"increment"=increment))
+    dominant_height <- if(age<age2) (dominant_height+increment) else (dominant_height-increment)
+  }
+
+  return(
+    hd_df[,"height"][nrow(hd_df)]
+  )
+}
+
+#Time to reach breast height, neg. exp.
+# PineBh <- data.frame(SI100=seq(30,9,-3),t1.3=c(9,10,11,12,14,17,19,23))
+# SpruceBh <- data.frame(SI100=seq(30,12,-3),t1.3=c(11,12,13,15,17,20,23))
+#
+# PineBhYr <- PineBh %>% lm(formula = log(t1.3)~SI100)
+# SpruceBhYr <- SpruceBh %>% lm(formula = log(t1.3)~SI100)
+#
+# PineBh %>% mutate(Pred=exp(predict(PineBhYr)))
+# SpruceBh %>% mutate(Pred=exp(predict(SpruceBhYr)))
+
+#' Smoothing of time required to reach breast height according to Gustavsen 1980.
+#' @details Both species modelled with log-linear function, adj. R^2>0.98
+#' @param SI100 Site index 100, according to Gustavsen 1980.
+#' @param species One of 'Picea abies' (default) or 'Pinus sylvestris'.
+#' @return Number of years required to reach breast height 1.3m.
+#' @export
+#' @author Carl Vigren SLU
+Gustavsen_1980_time_to_breast_height <- function(SI100,species="Picea abies"){
+  if(species=="Picea abies") return(exp(3.607141 - 0.041699*SI100))
+
+  if(species=="Pinus sylvestris") return(exp(3.486402 -0.044592*SI100))
+}
---
+++ R/Heiskanen_2018_SIS_Finland.R
@@ -0,0 +1,48 @@
+#' Correlation between H100 and site factors in Finland
+#'
+#' @source Heiskanen J., Hallikainen V., Uusitalo J., Ilvesniemi H. (2018).
+#' Co-variation relations of physical soil properties and site characteristics
+#' of Finnish upland forests. Silva Fennica vol. 52 no. 3 article id 9948.
+#' \url{https://doi.org/10.14214/sf.9948}
+#'
+#' @details Multiple R^2 = 0.829. adj.R^2 = 0.813. Residual SE= 2.75.
+#'
+#' @description OBSERVE: H100 is *tree-species independent*.
+#' Will assume that site type category 1 & 2 are aggregated & that
+#' site type 6 (Barren heath forests) have equivalent H100 to site type 5.
+#'
+#'
+#' @param site_type Numeric. 1 (Default): Herb-rich forests (groves), 2: herb-rich heath forests
+#' (OMT). 3: mesic heath forests (MT). 4. sub-xeric heath forests (VT). 5. xeric
+#' heath forests (CT). 6. Barren heath forests. (ClT). [forester::Finland_vegetation_types()]
+#' @param fine_fraction_percentage Fine fraction (<0.063 mm, %). Min (2.9). Max (92.57). Mean (32.74). Median (30.98).
+#' @param temperature_sum Degree days, at 2m, threshold 5 degrees Celsius. Min (807). Max (1746). Mean (1383.91). Median (1425.5).
+#'
+#' @md
+#' @return H100 Estimate. (tree-species independent).
+#' @export
+#'
+#' @examples
+Heiskanen_2018_SIS_Finland <- function(
+  site_type=1,
+  temperature_sum=1425.5,
+  fine_fraction_percentage=32.74
+){
+
+  stopifnot(site_type%in%seq(1,6,1))
+  stopifnot(fine_fraction_percentage>0 && fine_fraction_percentage<=100)
+
+  mesic <- ifelse(site_type==3,1,0)
+  subxeric <- ifelse(site_type==4,1,0)
+  xeric <- ifelse(site_type%in%c(5,6),1,0)
+
+
+  return(
+    0.848+
+      -5.96*mesic+
+      -4.409*subxeric+
+      -6.962*xeric+
+      +0.017*temperature_sum+
+      +0.071*fine_fraction_percentage
+  )
+}
