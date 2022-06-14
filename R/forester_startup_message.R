bc73b2d3b91d6d5d768972036cced35918b87d67
Carl Vigren <carl.vigren@slu.se>
2022-06-14 12:57:53 +0200

    Vignette Hoffmann
    Added the Hoffmann vignette.

--- R/forester_startup_message.R
+++ R/forester_startup_message.R
@@ -1,3 +1,3 @@
 .onAttach <- function(libname, pkgname) {
-  packageStartupMessage("forester is being actively developed.\nFor News and issues, go to 'https://github.com/Silviculturalist/forester'\nBreaking changes can take place at any time.\n\n Saturday 4 June: Massive update to structure of Soderbergs Individual Tree functions.\n")
+  packageStartupMessage("forester is being actively developed.\nFor News and issues, go to 'https://github.com/Silviculturalist/forester'\nBreaking changes can take place at any time.\n\n Saturday 4 June: Massive update to structure of Soderbergs Individual Tree functions.\n Tuesday 14 June: New data & vignette available! ?Hoffmann_1982")
 }
