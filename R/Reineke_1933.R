9de9b4419adcad64615d87a403488d47081d59ce
Carl Vigren <carl.vigren@slu.se>
2023-12-15 16:51:26 +0100

    Added documentation for Reineke 1933 dataset and removed leftovers from Elfving basal area function

---
+++ R/Reineke_1933.R
@@ -0,0 +1,11 @@
+#' Data from Reineke article on SDI, 1933.
+#' @source Reineke, L. H. 1933. Perfecting a stand-density index for even-aged forests. Journal of Agricultural Research. Vol. 46. Number 7. Washington, D.C.
+#' Available Online: \url{https://www.fs.usda.gov/psw/publications/cfres/cfres_1933_reineke001.pdf}
+#' @format A dataframe with the following columns:
+#' \describe{
+#' \item{AverageDiameterInches}{QMD}
+#' \item{NperAcre}{Stems per acre}
+#' \item{SPP}{Species}
+#' }
+#' @description Data digitised from graphs from original source.
+"Reineke_1933"
